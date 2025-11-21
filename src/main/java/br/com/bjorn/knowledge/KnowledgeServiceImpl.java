37d8e88c30f2a59fa0b27e0dec60ff9343865d52
EnmaRyumaa <fer.augustofs@gmail.com>
2025-11-21 20:51:00 -0300

    Remoção de lombok/docker

--- src/main/java/br/com/bjorn/knowledge/KnowledgeServiceImpl.java
+++ src/main/java/br/com/bjorn/knowledge/KnowledgeServiceImpl.java
@@ -1,5 +1,10 @@
 package br.com.bjorn.knowledge;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -8,11 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Service
 @Transactional
