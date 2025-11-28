a9f60fbff5daf50cf2769a9aa862895466a90b47
Fernando Augusto <fer.augustofs@gmail.com>
2025-11-28 11:23:51 -0300

    Cors permitindo tudo

--- src/main/java/br/com/bjorn/config/CorsConfig.java
+++ src/main/java/br/com/bjorn/config/CorsConfig.java
@@ -15,10 +15,10 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:3000", "http://127.0.0.1:4200", "http://127.0.0.1:3000", "http://bjorn-front.s3-website-us-east-1.amazonaws.com"));
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setAllowedMethods(List.of("*"));
         corsConfig.setAllowedHeaders(List.of("*"));
-        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowCredentials(false);
         corsConfig.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
