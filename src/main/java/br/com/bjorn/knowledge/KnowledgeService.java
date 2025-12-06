69a88ef1068f5b84f7b1df02ee650896c598513c
oifernando <101755104+oifernando0@users.noreply.github.com>
2025-12-06 12:57:53 -0300

    Make knowledge PDF indexing reactive

--- src/main/java/br/com/bjorn/knowledge/KnowledgeService.java
+++ src/main/java/br/com/bjorn/knowledge/KnowledgeService.java
@@ -1,11 +1,12 @@
 package br.com.bjorn.knowledge;
 
 import org.springframework.http.codec.multipart.FilePart;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface KnowledgeService {
-    void indexPdf(FilePart file, String specialist);
+    Mono<Void> indexPdf(FilePart file, String specialist);
     List<KnowledgeChunk> searchRelevantChunks(String specialist, String question);
     int getLastMaxScore();
     int getMinAcceptableScore();
